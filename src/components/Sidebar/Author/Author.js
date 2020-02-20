8ab85fe35c6aba07a48f5cc30f2b427ab2258e19
tivie001 <59585410+tivie001@users.noreply.github.com>
2020-02-19 23:56:40 -0700

    Add files via upload

--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -16,24 +16,24 @@ const Author = ({ author, isIndex }: Props) => (
   <div className={styles['author']}>
     <Link to="/">
       <img
-        src={withPrefix(author.photo)}
+        src="http://localhost:8000/profile-pic.jpg"
         className={styles['author__photo']}
-        width="75"
-        height="75"
-        alt={author.name}
+        width="110"
+        height="100"
+        alt="Tyler Ivie"
       />
     </Link>
 
     { isIndex === true ? (
       <h1 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
+        <Link className={styles['author__title-link']} to="/">Tyler Ivie</Link>
       </h1>
     ) : (
       <h2 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
+        <Link className={styles['author__title-link']} to="/">Tyler Ivie</Link>
       </h2>
     )}
-    <p className={styles['author__subtitle']}>{author.bio}</p>
+    <p className={styles['author__subtitle']}>Welcome to my blog! Consume at your own pleasure.</p>
   </div>
 );
 
